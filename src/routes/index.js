15b83a083e245e76d8664fd41e3e358140c1808f
meza <meza@meza.hu>
2015-07-09 20:56:40 +0100

    extracted register handler #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,40 +21,6 @@ module.exports = function (server) {
 		},
 		method: 'POST',
 		path: '/endpoints',
-		handler: function (req, reply) {
-
-			server.route({
-				method: req.payload.method || 'GET',
-				path: req.payload.endpoint,
-				handler: function (subreq, resp) {
-
-					var finalResponse = {
-							body: '',
-							code: 200,
-							headers: {}
-						},
-						response,
-						name;
-
-					if (req.payload.response) {
-						finalResponse.body = req.payload.response.body || '';
-						finalResponse.code = req.payload.response.code || 200;
-						finalResponse.headers = req.payload.response.headers || {};
-					}
-
-					response = resp(finalResponse.body)
-						.code(finalResponse.code).hold();
-
-					for (name in finalResponse.headers) {
-						if (finalResponse.headers.hasOwnProperty(name)) {
-							response.header(name, finalResponse.headers[name]);
-						}
-					}
-					response.send();
-				}
-			});
-
-			reply().code(201);
-		}
+		handler: require('../lib/register')(server)
 	});
 };
