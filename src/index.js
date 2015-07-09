b4ad34442769cc4a5ca7afda32f49fada1ebb607
meza <meza@meza.hu>
2015-07-09 20:37:30 +0100

    added hapi-swagger for api docs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 'use strict';
 
 var Hapi = require('hapi'),
-	server = new Hapi.Server();
+	server = new Hapi.Server(),
+	swaggerOptions = {};
 
 server.connection({port: 8080});
 
 require('./routes')(server);
 
+server.register({
+	register: require('hapi-swagger'),
+	options: swaggerOptions
+}, function (err) {
+	if (err) {
+		server.log(['error'], 'hapi-swagger load error: ' + err);
+	} else {
+		server.log(['start'], 'hapi-swagger interface loaded');
+	}
+});
+
 if (!module.parent) {
-	server.start(function () {});
+	server.start(function () {
+	});
 }
 
 module.exports = server;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,24 @@
 'use strict';
 
+var Joi = require('joi');
+
 module.exports = function (server) {
 	server.route({
+		config: {
+			tags: ['api'],
+			description: 'Create endpoints on the fly',
+			validate: {
+				payload: Joi.object().keys({
+					endpoint: Joi.string().required(),
+					method: Joi.string().optional(),
+					response: Joi.object().keys({
+						code: Joi.number().optional(),
+						headers: Joi.object().optional().unknown().description('Key:value pair'),
+						body: Joi.string().optional()
+					}).unknown()
+				}).unknown()
+			}
+		},
 		method: 'POST',
 		path: '/endpoints',
 		handler: function (req, reply) {
